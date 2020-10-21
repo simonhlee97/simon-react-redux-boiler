19200abcdd65cd7a1ef389c028a301e5aca10b3d
Simon Lee <simonhlee97@hotmail.com>
2020-10-21 13:16:59 +0900

    updating the header (146)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+// conditionally show navLinks
+import { connect } from 'react-redux'
 
 class Header extends Component {
+	renderNavlinks() {
+		if (this.props.authenticated) {
+			return (
+				<div>
+					<Link to="/signout">Sign out</Link>
+					<Link to="/profile">Profile</Link>
+				</div>
+			)
+		} else {
+			return (
+				<div>
+					<Link to="/signup">Sign up</Link>
+					<Link to="/signin">Log in</Link>
+				</div>
+			)
+		}
+	}
 	render() {
 		return (
 			<div>
 				<Link to="/">Simon Boiler-React</Link>
-				<Link to="/signup">Sign up</Link>
-				<Link to="/signin">Log in</Link>
-				<Link to="/signout">Sign out</Link>
-				<Link to="/profile">Profile</Link>
+				{this.renderNavlinks()}
 			</div>
 		)
 	}
 }
-
-export default Header
+function mapStateToProps(state) {
+	return { authenticated: state.auth.authenticated }
+}
+export default connect(mapStateToProps)(Header)
